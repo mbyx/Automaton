542b81b19d3fb04484dbab7761e5fd7496993281
Alee Azam <68140787+aleeazam@users.noreply.github.com>
2021-05-27 23:18:11 +0500

    add docustring

--- automaton/__init__.py
+++ automaton/__init__.py
@@ -1,3 +1,10 @@
+""" 
+Automaton is a collection of libraries and custom modules designed for making automation interfaces in Linux easier.
+For more, check out https://github.com/Abdul-Muiz-Iqbal/Automaton 
+"""
+
+# This will require you to add automaton-linux to PyPI again
+
 from .lib import *
 from .menu import *
 from .window import *
