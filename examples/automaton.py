9522448b6afa2e3a492664198969958b0d0893e7
Abdul-Muiz-Iqbal <parkermuiz0@gmail.com>
2021-11-30 14:25:11 +0500

    Fixed #28

--- examples/automaton.py
+++ examples/automaton.py
@@ -1,5 +1,6 @@
-from automaton.core import Automaton, Key, Button
 import datetime
+
+from automaton import Automaton, Button, Key
 
 # Create an instance of Automaton.
 # WARNING: Automaton works by taking control over your keyboard and mouse devices,
@@ -12,13 +13,16 @@ import datetime
 # for device in map(evdev.InputDevice, evdev.list_devices()):
 #     print(device.name, '::', device.path)
 # Replace with your own device paths. These correspond to my keyboard and mouse.
-app = Automaton.new(devices = ['/dev/input/event6', '/dev/input/event5']) # Pass in the path to the keyboard and mouse.
+app = Automaton.new(
+    devices=["/dev/input/event6", "/dev/input/event5"]
+)  # Pass in the path to the keyboard and mouse.
 
 # Hotstrings are registered by decorating a function.
 # The trigger string allows unicode. The string returned (if any) is typed. Unicode is also supported for the typed string.
 @app.on(":date")
 def get_date():
-    return f'{datetime.date.today()}'
+    return f"{datetime.date.today()}"
+
 
 # Hotkeys are also registered by decorating a function.
 # The triggers can be numerous, but are sorted by their scancode. Meaning Shift+A and A+Shift are both allowed.
@@ -26,14 +30,18 @@ def get_date():
 def shift_a():
     print("Who pressed me?")
 
+
 # There are different modifiers for each action. You can get these from automaton.actions:
-from automaton.actions import HotStringOptions, HotKeyOptions, RemapOptions
+from automaton.actions import HotKeyOptions, HotStringOptions, RemapOptions
+
+
 # HotStringOptions.CaseSensitive
 # HotStringOptions.TriggerInsideWord Allows hotstring to be triggered even if the trigger text is inside the currently typed word
 # Eg:
-@app.on("btw", options = [HotStringOptions.TriggerInsideWord])
-def btw(): # Typing shlbtw will expand to shl by the way
+@app.on("btw", options=[HotStringOptions.TriggerInsideWord])
+def btw():  # Typing shlbtw will expand to shl by the way
     return "by the way"
+
 
 # HotStringOptions.TriggerImmediately When applied, does not require a trigger key to activate
 # HotStringOptions.PreventAutoBackspace When a hotstring is triggered and is a text replacement, do not auto backspace.
@@ -49,7 +57,7 @@ app.remap(Key.Numpad4, Button.LeftButton)
 # Hotstrings, Hotkeys and remaps have a keyword arg called register_if. These actions
 # are activated if register_if() returns True. Otherwise, the action never happens.
 # Remaps K to A only IF 1 == 2. So never.
-app.remap(Key.K, Key.A, context = lambda: 1 == 2)
+app.remap(Key.K, Key.A, when=lambda: 1 == 2)
 
 # You can also emit callbacks when any key is pressed or released:
 app.device.on_press(lambda event: print(event.code))
@@ -58,4 +66,4 @@ app.device.on_release(lambda event: print(event.code))
 # Multiple callbacks are allowed.
 app.device.on_press(lambda event: print(event.value))
 
-app.run() # Start listening and redirecting events. Hotkeys and such won't work without this.
+app.run()  # Start listening and redirecting events. Hotkeys and such won't work without this.
